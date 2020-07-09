4c487c8001a89b76d2b3fcc4fa177e7c11f7c892
jfirus <firusjesica@gmail.com>
2020-07-09 11:39:47 -0300

    Ajuste en el código del detalle de las recetas, en el ejs, para que tome de ahi los ingredientes y los pasos

--- site/src/controller/recetasController.js
+++ site/src/controller/recetasController.js
@@ -13,13 +13,7 @@ const controller ={
         
         //Buscar el ID de la receta que viene por parámeto
         let receta = recetasModel.findById(req.params.recetaId);
-        
-        let ingredientes = receta.ingredientes.split('|');
-        let pasos = receta.pasos.split('|');
-
-        console.log(ingredientes);
-        console.log(pasos);
-        
+       
         /* Para validar si tiene contenido la variable
         if (receta){
             console.log('Tiene receta');
@@ -27,7 +21,7 @@ const controller ={
             console.log('No tiene receta')
         }
         */
-        return res.render('receta-detail', {receta, ingredientes, pasos});
+        return res.render('receta-detail', {receta});
     }
 };
 module.exports = controller;
