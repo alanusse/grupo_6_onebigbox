24e2ff095bb4c19265e584cbdcb3f3c0895f31cf
jfirus <firusjesica@gmail.com>
2020-08-14 18:16:48 -0300

    Solución problema que cuando se producía un error en cualquier parte del sistema siempre mostraba la cabecera de la página, ahora si se produce un error en el /admin, muestra la cabecera del admin

--- site/src/app.js
+++ site/src/app.js
@@ -69,8 +69,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  let admin = req.headers.referer.includes('admin')
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {admin});
 });
 
 
