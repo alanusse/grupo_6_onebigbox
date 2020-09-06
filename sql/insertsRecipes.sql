6c3da3f1c8f82f8826ad8ceb79b2a3587abae8dc
facumartinn <facumartinn@gmail.com>
2020-09-06 19:51:58 -0300

    recetas nuevas

--- sql/insertsRecipes.sql
+++ sql/insertsRecipes.sql
@@ -1,4 +1,149 @@
 -- **** SENTENCIA DE INSTERT DE RECETAS ******
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Raviolis de quesos con boloñesa',
+'Un plato donde se combinan los mejores quesos de montaña con la salsa más famosa. Un plato repleto de energía y probióticos del queso, que benefician a tu microbiota intestinal.',
+'1.Pon un cazo con agua a hervir. Corta la cebolla en dados. Lava y corta la zanahoria en 4 bastones y luego en láminas finas (No hace falta pelar). Corta el apio en láminas. |2. Pon una sartén a fuego medio con un chorrito de aceite. Añade la zanahoria y el apio y cocina durante 2-3 minutos.|3. Añade la cebolla y cocina durante 4-5 minutos más. Añade la carne y remueve durante 3-4 minutos. |4. Una vez el agua esté hirviendo, añade la pasta y cocina durante el tiempo que ponga en la caja. |5. Añade el tomate frito, la hierbas, pimienta y una pizca de sal a la sartén. |6. Cuela la pasta y añádela a la sartén. Mezcla todo bien y emplata. Usa el queso rallado para espolvorear por encima.',
+'Carne picada de ternera|Zanahoria|Cebolla|Tomate frico casero|Queso emmental rallado fundido|Hierbas provenzales|Apio|Raviolis de quesos de montaña',
+20,
+100,
+1,
+'Raviolis.jpg'
+);
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Risotto de azafrán y pollo',
+'La intensidad del azafrán combina de maravilla en este plato tan sencillo y delicioso. La carne de pollo aporta una gran cantidad de vitaminas del grupo B que ayudan al organismo a extraer el máximo de energía, que proporciona el arroz en este caso.',
+'1. Corta la pechuga el pollo en tiras y después haz dados de unos 2-3 cm. Pela y corta la cebolla en dados más pequeños que el pollo. |2. Calienta a fuego medio una cuharada de aceite de oliva en una sartén y añade la cebolla y el pollo. Rehoga hasta que esté dorado el pollo removiendo continuamente. Cuando lo tengas baja el fuego al mínimo y continúa. |3. En una olla o cazo calienta el caldo de pollo. Cuando lo tengas listo separa un vaso de caldo y agrégale el azafrán. Añade el arroz a la sartén y remueve. |4. Añade el caldo normal y de azafrán de poco a poco con la ayuda de un cucharón. Remueve constantemente minetras esperas a que se evapore el caldo. |5. Repite la acción hasta terminar el caldo y quede el arroz al dente. |6. Agrega la mantequilla y el parmesano. Remueve enérgicamente hasta integrar todos los ingredientes y retira del fuego.',
+'Caldo de pollo|Azafrán|Mantequilla|Arroz redondo|Contramuslos de pollo|Grana padano|Cebolla',
+35,
+100,
+1,
+'risotto.jpg'
+);
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Pollo con salsa de champiñones',
+'Una salsa de champiñones clásica y simple, ¡pero nunca falla! En este plato el pollo es el encargado de aportar proteínas de calidad y vitaminas del grupo B, como la niacina que ayuda al organismo a acceder a la energía y disminuir la fatiga.',
+'1. Pon el horno a calentar a 200ºC. Pon un cazo con agua a hervir. Corta las zanahorias en bastones. Coloca las zanahorias en una bandeja de horno. Colócalas en un lado dejando hueco para las pechugas. Echales un chorrito de aceite y una pizca de sal e introdúcelas 20-25 minutos. Puedes usar papel de horno si quieres ensuciar menos. |2. Corta los champiñones en láminas. Corta el pollo en 2 mitades con un corte longitudinal. Corta las patatas en dados, no hace falta pelarlas. |3. Añade las patatas al cazo y cuece durante 12 minutos. Cuando las zanahorias lleven 15 minutos, saca la bandeja, coloca el pollo, echa una pizca de sal y cocina todo durante 10 minutos. Cuando estén saca del horno y reserva. |4. Coloca una sartén a fuego medio con un chorrito de aceite. Cuando esté caliente añade los champiñones y saltea durante 3-4 minutos. |5. Añade 3/4 de nata (reserva un chorrito para el puré) y la salsa de soja a la sartén. Añade una pizca de sal y otra de pimienta y remueve. Cocina durante 3-4 minutos y tendrás tu salsa. |6. Cuela las patatas y devuélvelas a la olla. Ponla a fuego bajo y añade la nata restante y la mantequilla. Chafa con una prensa de patatas o con un tenedor, también puedes hacerlo en una batidora. Coloca el pollo, el puré y las zanahorias en un plato y añade la salsa por encima.',
+'Pollo|Champiñon Portobello|Nata para cocinar|Papa|Zanahoria|Tamari|Mantequilla',
+30,
+100,
+1,
+'pollochampinon.jpg'
+);
+
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Macarrones de trigo sarraceno con albóndigas a la manzana',
+'El toque de manzana rallada en la carne de ternera (con minerales que mantienen la salud sanguínea). El trigo de sarraceno ademas es fuente de fibra que promueve la salud digestiva y del corazón.',
+'1. Pela y ralla la manzana. En un bol grande, coloca la carne picada, la manzana rallada, el grana padano, las hierbas provenzales y una pizca de sal. Mezcla todo. |2. Divide la mezcla con la carne picada en pequeñas albóndigas dándole forma con tus manos. |3. Pon una sartén con un chorrito de aceite de oliva a calentar a fuego medio-fuerte. Cuando esté caliente, añade las albóndigas y cocina 3-4 minutos. Mientras, pon una olla con agua y sal a calentar. |4. Una vez rompa el agua a hervir, añade la pasta y deja cocer durante 8 minutos. Después, escurre y reserva. |5. Pela y pica en dados finos la cebolla. Pela y pica en dados finos el ajo. Pica en trozos más finos la albahaca y reserva. En la misma sartén donde cocinaste las albóndigas, añade un chorrito de aceite y cuando esté caliente, cocina la cebolla y el ajo a fuego medio durante 3-4 minutos. |6. Por último, espolvorea el orégano y aliña rociando aceite de oliva y sal gorda al gusto.',
+'Manzana|Macarrones trigo sarraceno|Albahaca|Cebolla|Tomate frito casero|Ajo|Carne picada de ternera',
+30,
+100,
+2,
+'macarrones.jpg'
+);
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Tortilla 3 quesos',
+'Una tortilla con mucho queso deliciosa para cualquiera con una ensalada de pera que aporta frescura. Esta receta es muy rica en proteína de origen animal, calcio, hierro y fibra.',
+'1. Corta la pera en dados, si quieres con la piel. Rompe las nueces, aun dentro de la bolsa,con ayuda de un vaso. |2. En un bol coloca las espinacas, la pera y las nueces. |3. Calienta una sartén antiadherente a fuego medio bajo con un chorrito de aceite de oliva. Casca los huevos en un bol o plato hondo. |4. Añade los quesos desmenuzados al plato con el huevo y mezcla bien. Añade una pizca de sal. |5. Añade la tortilla a la sartén, en una tanda o en más tandas si prefieres hacer varias tortillas más pequeñas. |6. Dobla la tortilla por la mitad cuando lleve 1 minuto y deja hacerse otro minuto más. Emplata y sirve junto con la ensalada aliñada.',
+'Queso camembert|Queso de cabra|Queso emmental|Huevos|Nueces|Pera|Espinaca',
+15,
+100,
+2,
+'tortilla.jpg'
+);
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Garbanzos con verduras y salsa de limón y coco',
+'¡Le damos un toque veraniego a este clásico plato indio! Este plato de garbanzos vegano, rápido, sabroso y saciante, es muy rico en proteínas vegetales, fibra y compuestos presentes en la cúrcuma que aportan propiedades antiinflamatorias y anticancerigenas.',
+'1. Precalienta el horno a 200ºC. Pon a calentar un cazo con agua y una pizca de sal. Lava y corta las patatas en gajos. |2. Una vez rompa el agua a hervir, añade las patatas y deja cocinar 10 minutos. Después, escurre y reserva. Mientras, corta la cebolla en juliana fina (tiras). Lava y corta los tomates a la mitad. Pica las espinacas y la hierbabuena en trozos más finos. Corta la coliflor en ramilletes pequeños |3. Pon una sartén a calentar. Escurre los garbanzos y lávalos con agua fría hasta que deje de salir espuma. En una bandeja de horno, coloca los ramilletes de coliflor y las patatas cocidas junto con una cucharada de aceite de oliva, la cúrcuma y el comino y mezcla bien para impregnar los ingredientes. Añádelo al horno 10-15 minutos hasta que estén cocinadas. |4. Cuando la sartén esté caliente, añade la cebolla junto con una pizca de sal y cocina un par de minutos hasta que tome un aspecto dorado. Después, añade los garbanzos y la pasta de curry y cocina todo un minuto más. Después, añade los tomates y las espinacas y cocina todo junto 2 minutos más. |5. Mientras, haz la salsa de limón y coco. Coloca la leche de coco en un bol y ralla y exprime el jugo del limón con tus manos. Mezcla todo. |6. Añade la salsa de coco a la sartén y deja reducir un par de minutos.Saca las verduras del horno y emplata junto con los garbanzos.',
+'Coliflor|Limón|Leche de coco|Cebolla|cúrcuma|Papa|Espinaca|Tomate Cherry|Comino|Pasta de curry|Hierbabuena|Garbanzos Cocidos',
+25,
+100,
+2,
+'garbanzos.jpg'
+);
+
+
+
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Ensalada Horiatiki',
+'Prepárate para teletransportarte a la auténtica Grecia con esta combinación de sabores e ingredientes mediterráneos. Una receta sencilla cargada de vitamina C, fitoquímicos y ácidos grasos que cuidan de tu salud cardiovascular y mantienen jóvenes tus células.',
+'1. Corta el pepino por la mitad longitudinalmente y después en medias lunas. Corta la cebolla por la mitad y luego en juliana fina. |2. Corta el pimiento por la mitad eliminando las semillas. Ahora corta el pimiento en tiras transversales, más finas o gruesas dependiendo de gustos. |3. Corta los tomates por la mitad y luego cada mitad en 4-6 partes dependiendo del grosor deseado. |4. Escurre, si llevan agua, las aceitunas negras. En un bol coloca todos los ingredientes que acabas de cortar. |5. En este paso tienes dos opciones: desmigar el feta por encima con la ayuda de tus dedos o colocarlo encima de la ensalada para luego romperlo con los cubiertos cuando vayas a servirlo. |6. Por último, espolvorea el orégano y aliña rociando aceite de oliva y sal gorda al gusto.',
+'Oregano|Queso Feta|Aceituna Negra|Tomate|Pepino|Morron|Cebolla',
+15,
+100,
+3,
+'ensaladahoriatiki.jpg'
+);
+
 INSERT INTO recipes (
     titulo ,
     description,
@@ -15,6 +160,26 @@ VALUES (
 'Carne picada de ternera|Tomate cherry|Vinagre balsámico|Tomate seco deshidratado|Brotes espinaca|Pasas sultanas|Rúcula|Queso camembert|Cebolla',
 30,
 100,
-2,
+3,
 'image1.png'
+);
+
+INSERT INTO recipes (
+    titulo ,
+    description,
+    pasos,
+    ingredientes,
+    tiempopreparacion ,
+    precio,
+    planId,
+    image)
+VALUES (
+'Ensalada de berenjena y queso vegano',
+'Llena de sabores intensos, esta ensalada ideal para cuidar de tu microbiota. La fibra de los vegetales sirven de sustrato mientras que el queso proporciona efectos cardio-saludables y colabora con el sistema inmune.',
+'1. Pon el horno a calentar a 200ºC. Lava y corta la berenjena en dados de 3cm. |2. Coloca la berenjena cortada sobre una bandeja de horno y rocía con un chorrito de aceite de oliva. Dale un toque de sal y pimienta e introduce en el horno durante 25 minutos o hasta que tengan un aspecto dorado. Puedes utilizar papel de horno o papel de aluminio para ensuciar lo menos posible |3. Lava y corta los tomates cherry en cuartos. Pela y pica la chalota en dados finos. Pica la hierbabuena en trozos pequeños. |4. En bol bol, mezcla una cucharada sopera de aceite de oliva, el vinagre balsámico y la hierbabuena picada. ¡Este es tu aderezo! |5. Cuando queden 5 minutos para que estén las berenjenas, añade las pasas y los piñones a la bandeja de horno. En un bol grande, coloca la mezcla de lechugas como base de la ensalada. Después, añade los tomates cherry, la chalota picada, las pasas, los piñones y la berenjena asada. |6. Por último, corta el queso en dados medianos, ponlo sobre la ensalada y dale sabor con el aliño de hierbabuena. ¡Ya lo tienes!',
+'Berenjena|Tomate Cherry|Lechuga|Chalotas|Queso vegano|Pasas|Vinagre balsámica',
+35,
+100,
+3,
+'ensaladaberengena.jpg'
 );
